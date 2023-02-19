2ec2cb5c3354849aade3c4c963b4b4a553ca403c
Faleye Oluwafemi joseph <faleyeoluwafemi1@gmail.com>
2023-02-19 14:06:40 +0100

    added get user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,14 +94,19 @@ const signupUser = async (req, res, next) => {
 // @desc Get user profile
 // @route GET /api/users/:id/profile
 // @access Private
-const getUserProfile = async (req, res) => {
-  const user = await User.findById(req.params.id).select('-password -__v');
+const getUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password -__v');
 
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: 'User not found' });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    next(error)
   }
+ 
 };
 
 // @desc Update user profile
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { signupUser } = require('../controllers/userController');
+const { signupUser, getUserProfile } = require('../controllers/userController');
+const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 /* Register user*/
 router.post('/signup', signupUser);
+router.get('/:id/profile', protect, getUserProfile);
 
 module.exports = router;
